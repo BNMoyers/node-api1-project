ee4c733c000628490109e2b545a139ef2af6150d
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-28 16:27:41 -0500

    delete working

--- index.js
+++ index.js
@@ -48,7 +48,17 @@ server.get("/users/:id", (req, res) =>{
     .catch(() =>{
         res.status(500).json({ error: "The user information could not be retrieved." })
     })
-})
+});
+
+server.delete("/users/:id", (req, res) =>{
+    db.remove(req.params.id)
+    .then(user => {
+        user ? res.status(200).json(user) : res.status(404).json({ message: "The user with the specified ID does not exist." })
+    })
+    .catch(() => {
+        res.status(500).json({ error: "The user could not be removed" })
+    })
+});
 
 /*listener*/
 const port = 5000;
