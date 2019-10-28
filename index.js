2dfbd365d99f886e1182f8c148ddab5858f3cfba
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-28 15:23:24 -0500

    C:/Program Files/Git/users get request complete and tested

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 /*dependencies*/
 const express = require('express');
+const db = require('./data/db.js');
 
 /*server*/
 const server = express();
 
 /*request handlers */
+server.get('/users', (req,res) => {
+    db.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'failed to get users '})
+    });
+});
 
 /*listener*/
 const port = 5000;
-server.listen(port, () => console.log('test API - Hello'))
+server.listen(port, () => console.log('test API running on port 5000'))
