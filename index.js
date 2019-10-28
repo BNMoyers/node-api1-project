ea6ce087cbc988d24d867238d6073ad5eaa0df9c
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-28 16:07:01 -0500

    tested and bug fixed post request

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 /*dependencies*/
-const express = require('express');
-const db = require('./data/db.js');
+const express = require("express");
+const db = require("./data/db.js");
 
 /*server*/
 const server = express();
-server.use(express.json())
+server.use(express.json());
 
 /*request handlers */
-server.post('/users', (req, res) =>{
-    const { name, bio } = req.body;
+server.post("/users", (req, res) => {
+  const { name, bio } = req.body;
 
-    (!name || !bio ) ? {
-        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
-    } :
-    db.insert(req.body)
-    .then(user => {
-        res.status(201).json(user)
-    })
-    .catch(err => {
-        res.status(500).json({ error: "There was an error while saving the user to the database"  })
-    })
-})
+  (!name || !bio)
+    ? res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user" })
+    : db
+        .insert(req.body)
+        .then(user => {
+          res.status(201).json(user);
+        })
+        .catch(() => {
+          res
+            .status(500)
+            .json({
+              error: "There was an error while saving the user to the database"
+            });
+        });
+});
 
-server.get('/users', (req,res) => {
-    db.find()
+server.get("/users", (req, res) => {
+  db.find()
     .then(users => {
-        res.status(200).json(users);
+      res.status(200).json(users);
     })
     .catch(err => {
-        res.status(500).json({ error: 'The users information could not be retrieved. '})
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved. " });
     });
 });
 
 /*listener*/
 const port = 5000;
-server.listen(port, () => console.log('test API running on port 5000'))
+server.listen(port, () => console.log("test API running on port 5000"));
