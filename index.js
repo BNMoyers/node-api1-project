b84c6e556d1e8c17b72cf22e56255295d8bbb320
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-28 15:35:32 -0500

    created post request

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ const db = require('./data/db.js');
 
 /*server*/
 const server = express();
+server.use(express.json())
 
 /*request handlers */
+server.post('/users', (req, res) =>{
+    const { name, bio } = req.body;
+
+    (!name || !bio ) ? {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } :
+    db.insert(req.body)
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({ error: "There was an error while saving the user to the database"  })
+    })
+})
+
 server.get('/users', (req,res) => {
     db.find()
     .then(users => {
         res.status(200).json(users);
     })
     .catch(err => {
-        res.status(500).json({ error: 'failed to get users '})
+        res.status(500).json({ error: 'The users information could not be retrieved. '})
     });
 });
 
